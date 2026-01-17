fc65657d2aea08b9099f790f841028ede83abb1f
Cenk Cetin <dev.cenkcetin@gmail.com>
2026-01-17 20:11:27 +0300

    ..

--- Streamlit/app_pages/__init__.py
+++ Streamlit/app_pages/__init__.py
@@ -1,6 +1,6 @@
 # Streamlit Sayfaları
 from . import dashboard
-from . import odenç_islemleri
+from . import odunc_islemleri
 from . import iade_islemleri
 from . import uye_yonetimi
 from . import kitap_envanteri
@@ -9,7 +9,7 @@ from . import ayarlar
 
 __all__ = [
 	'dashboard',
-	'odenç_islemleri',
+	'odunc_islemleri',
 	'iade_islemleri',
 	'uye_yonetimi',
 	'kitap_envanteri',
